5202f65a2dd0227d2b77ba6ca4ecfeb703d67f10
SpasZahariev <spaszahariev54@gmail.com>
2018-10-14 18:48:44 +0100

    put a video background

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'CAD-SpasZahariev'`, () => {
+  it(`should have as title 'Im-back'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('CAD-SpasZahariev');
+    expect(app.title).toEqual('Im-back');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to CAD-SpasZahariev!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Im-back!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'CAD-SpasZahariev';
+
+  public sendAlert(): void {
+    console.log('Hello beautiful');
+  }
 }
